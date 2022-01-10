8bc9a667c0fdd9748f01768bc099291d90f4ec2e
AltStreetBets <84208757+AltStreetBets@users.noreply.github.com>
2022-01-11 00:38:30 +0100

    Update main.rs
    visible update

--- main.rs
+++ main.rs
@@ -1,5 +1,5 @@
 //#![windows_subsystem = "windows"]
-
+use std::{thread, time};
 use text_io::read;
 use fltk::{app, button, frame::*, input, prelude::*, window,};
 use fltk_theme::{WidgetTheme, ThemeType};
@@ -45,6 +45,21 @@ fn pricebased() {
 
     let impermanent_loss :f32 = ((step_1-(((step_2.sqrt())*price_af)+((step_3.sqrt())*price_bf)))/step_4)*100_f32;
     println!("Impermanent loss is: {:.2}%", impermanent_loss);
+
+    println!("Press 0 to reset values, press any other key to let the programme shit itself");
+    let exitchoice :i32 = read!();
+    if exitchoice == 0 {
+        main();
+    } else {
+        println!("shitting myself");
+        thread::sleep(time::Duration::from_millis(500));
+        let mut n = 5;
+        while n > 0 {
+            println!("{}", n);
+            n -= 1;
+            thread::sleep(time::Duration::from_millis(1000));
+        };
+    };
 }
 
 fn amountbased() {
@@ -88,7 +103,7 @@ fn amountbased() {
     let total_final = amount_af + amount_bf;
     let average_af = amount_af / total_final;
     let average_bf = amount_bf / total_final;
-    
+
     let step_11 :f32 = average_b*average_af+average_a*average_bf;
     let step_22 :f32 = (average_a*average_b*average_bf)/average_af;
     let step_33 :f32 = (average_a*average_b*average_af)/average_bf;
@@ -99,4 +114,18 @@ fn amountbased() {
     let impermanent_loss1 :f32 = ((step_11-(((step_22.sqrt())*average_af)+((step_33.sqrt())*average_bf)))/step_44)*100_f32;
     println!("Impermanent loss is: {:.2}%", impermanent_loss1);
 
+    println!("Press 0 to reset values, press any other key to let the programme shit itself");
+    let exitchoice :i32 = read!();
+    if exitchoice == 0 {
+        main();
+    } else {
+        println!("shitting myself");
+        thread::sleep(time::Duration::from_millis(500));
+        let mut n = 5;
+        while n > 0 {
+            println!("{}", n);
+            n -= 1;
+            thread::sleep(time::Duration::from_millis(1000));
+        };
+    };
 }
