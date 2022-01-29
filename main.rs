3cef2fa82887442da9b8a67d08235d930df980dd
AltStreetBets <84208757+AltStreetBets@users.noreply.github.com>
2022-01-29 16:02:35 +0100

    Add files via upload

--- main.rs
+++ main.rs
@@ -1,131 +1,115 @@
-//#![windows_subsystem = "windows"]
-use std::{thread, time};
-use text_io::read;
-use fltk::{app, button, frame::*, input, prelude::*, window,};
-use fltk_theme::{WidgetTheme, ThemeType};
-use fltk_flex::{Flex, FlexType};
+#![windows_subsystem = "windows"]
+use fltk::{app, prelude::*, window, input::{self, FloatInput}, group, frame, enums::CallbackTrigger, utils::filename_expand};
+
+
 
 fn main() {
+    
 
-    loop {
-        println!("price based (0) or amount based (1)");
-        let choice_number :i32 = read!();
+    let app = app::App::default();
+    let mut win = window::Window::default()
+        .with_size(750, 500)
+        .center_screen()
+        .with_label("Impermanent Loss Calculator");
 
-        if choice_number == 0 {
-            pricebased();
-            break;
-        } else if choice_number == 1 {
-            amountbased();
-            break;
-        } else {
-            println!("this wasn't a valid option");
-            continue;
-        };
-    };
+
+    let mut pack = group::Pack::default()
+        .with_size(200, 100)
+        .with_pos(150, 100);
+    let frame1 = frame::Frame::new(0, 0, 0, 50, "Token symbol");
+    let mut tokenname1 = input::Input::new(0, 0, 200, 20, "");
+    let frame2 = frame::Frame::new(0, 0, 0, 50, "Initial token amount");
+    let mut price_a = input::FloatInput::new(0, 0, 200, 20, "");
+    let frame_1 = frame::Frame::new(0, 0, 0, 50, "Final token amount");
+    let mut price_af = input::FloatInput::new(0, 0, 200, 20, "");
+
+    pack.end();
+
+    
+    let mut pack2 = group::Pack::default()
+        .with_size(200, 100)
+        .right_of(&pack, 50);
+    let frame3 = frame::Frame::new(0, 0, 0, 50, "Token symbol");
+    let mut tokenname2 = input::Input::new(0, 0, 200, 20, "");
+    let frame4 = frame::Frame::new(0, 0, 0, 50, "Initial token amount");
+    let mut price_b = input::FloatInput::new(0, 0, 200, 20, "");
+    let frame_2 = frame::Frame::new(0, 0, 0, 50, "Final token amount");
+    let mut price_bf = input::FloatInput::new(0, 0, 200, 20, "");
+
+    pack2.end();
+
+    
+
+
+    let mut frame5 = frame::Frame::default()
+        .with_size(400, 100)
+        .with_label("Impermanent loss is: 6.90%")
+        .below_of(&pack, 100);
+    
+    
+
+
+    win.make_resizable(true);
+    win.end();
+    win.show();
+
+    price_a.set_value(&format!("{}", 1.0));
+    price_b.set_value(&format!("{}", 1.0));
+    price_af.set_value(&format!("{}", 2.14));
+    price_bf.set_value(&format!("{}", 0.9955));
+    tokenname1.set_value(&format!{"i.e. PEE"});
+    tokenname2.set_value(&format!{"i.e. BAN"});
+
+    price_a.set_trigger(CallbackTrigger::Changed);
+    price_b.set_trigger(CallbackTrigger::Changed);
+    price_af.set_trigger(CallbackTrigger::Changed);
+    price_bf.set_trigger(CallbackTrigger::Changed);
+
+
+    let (s, r) = app::channel::<bool>();
+
+    price_a.emit(s, true);
+    price_b.emit(s, true);
+    
+    price_af.emit(s, false);
+    price_bf.emit(s, false);
+
+
+    
+    while app.wait() {
+
+
+        //FUCK ERROR HANDLING
+        let price_a_clean :f64 = if price_a.value().is_empty() {1.0} else {price_a.value().parse().unwrap()};
+        let price_b_clean :f64 = if price_b.value().is_empty() {1.0} else {price_b.value().parse().unwrap()};
+        let price_af_clean :f64 = if price_af.value().is_empty() {price_a_clean} else {price_af.value().parse().unwrap()};
+        let price_bf_clean :f64 = if price_bf.value().is_empty() {price_b_clean} else {price_bf.value().parse().unwrap()};
+
+
+
+
+        match r.recv() {
+            Some(msg) => {
+                if msg {
+                    frame5.set_label(&format!("Impermanent loss is: {:.2}%", calculation(price_a_clean, price_b_clean, price_af_clean, price_bf_clean)));
+                } else {
+                    frame5.set_label(&format!("Impermanent loss is: {:.2}%", calculation(price_a_clean, price_b_clean, price_af_clean, price_bf_clean)));
+                }
+            }
+            None => (),
+
+        }
+    }
+
 }
 
-fn pricebased() {
-    println!("You choose price based");
-    println!("What was initial price of token A?");
-    let price_a :f32 = read!();
-
-    println!("What was initial price of token B?");
-    let price_b :f32 = read!();
+fn calculation(price_a :f64, price_b :f64, price_af :f64, price_bf :f64) -> f64 {
     
-    println!("What was final price of token A?");
-    let price_af :f32 = read!();
+    let step_1 :f64 = price_b*price_af+price_a*price_bf;
+    let step_2 :f64 = (price_a*price_b*price_bf)/price_af;
+    let step_3 :f64 = (price_a*price_b*price_af)/price_bf;
+    let step_4 :f64 = price_b*price_af+price_a*price_bf;
 
-    println!("What was final price of token B?");
-    let price_bf :f32 = read!();
-
-    let step_1 :f32 = price_b*price_af+price_a*price_bf;
-    let step_2 :f32 = (price_a*price_b*price_bf)/price_af;
-    let step_3 :f32 = (price_a*price_b*price_af)/price_bf;
-    let step_4 :f32 = price_b*price_af+price_a*price_bf;
-
-    let impermanent_loss :f32 = ((step_1-(((step_2.sqrt())*price_af)+((step_3.sqrt())*price_bf)))/step_4)*100_f32;
-    println!("Impermanent loss is: {:.2}%", impermanent_loss);
-
-    println!("Press 0 to reset values, press any other key to let the programme shit itself");
-    let exitchoice :i32 = read!();
-    if exitchoice == 0 {
-        main();
-    } else {
-        println!("shitting myself");
-        thread::sleep(time::Duration::from_millis(500));
-        let mut n = 5;
-        while n > 0 {
-            println!("{}", n);
-            n -= 1;
-            thread::sleep(time::Duration::from_millis(1000));
-        };
-    };
-}
-
-fn amountbased() {
-    println!("You choose amount based");
-    
-    println!("What was initial amount of token A?");
-    let mut amount_a :f32 = read!();
-
-    println!("What was initial amount of token B?");
-    let mut amount_b :f32 = read!();
-    
-
-    loop {
-        println!("Did you add more tokens? yes(0)");
-        let choicetoken :i32 =read!();
-
-        if choicetoken == 0 {
-            println!("What was the amount of token A?");
-            let amount_a1 :f32 = read!();
-            amount_a += amount_a1;
-
-            println!("What was the amount of token B?");
-            let amount_b1 :f32 = read!();
-            amount_b += amount_b1;            
-
-        } else {
-            break;
-        };
-    };
-
-
-    let amount_total = amount_a + amount_b;
-    let average_a = amount_a / amount_total;
-    let average_b = amount_b / amount_total;
-
-    println!("What was final amount of token A?");
-    let amount_af :f32 = read!();
-
-    println!("What was final amount of token B?");
-    let amount_bf :f32 = read!();
-    let total_final = amount_af + amount_bf;
-    let average_af = amount_af / total_final;
-    let average_bf = amount_bf / total_final;
-
-    let step_11 :f32 = average_b*average_af+average_a*average_bf;
-    let step_22 :f32 = (average_a*average_b*average_bf)/average_af;
-    let step_33 :f32 = (average_a*average_b*average_af)/average_bf;
-    let step_44 :f32 = average_b*average_af+average_a*average_bf;
-
-
-    //lol, I am such a nitwitt, you can just use the same formula. Anyways, was good practice.
-    let impermanent_loss1 :f32 = ((step_11-(((step_22.sqrt())*average_af)+((step_33.sqrt())*average_bf)))/step_44)*100_f32;
-    println!("Impermanent loss is: {:.2}%", impermanent_loss1);
-
-    println!("Press 0 to reset values, press any other key to let the programme shit itself");
-    let exitchoice :i32 = read!();
-    if exitchoice == 0 {
-        main();
-    } else {
-        println!("shitting myself");
-        thread::sleep(time::Duration::from_millis(500));
-        let mut n = 5;
-        while n > 0 {
-            println!("{}", n);
-            n -= 1;
-            thread::sleep(time::Duration::from_millis(1000));
-        };
-    };
+    let impermanent_loss :f64 = ((step_1-(((step_2.sqrt())*price_af)+((step_3.sqrt())*price_bf)))/step_4)*100_f64;
+    return impermanent_loss;
 }
